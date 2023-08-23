2e34f96b61bc897cc9d0daf8930e0a7ac2c80c75
AhmedSodagar1 <ahmed1sodagar@gmail.com>
2023-08-23 08:42:12 -0700

    ss

---
+++ git/app.js
@@ -0,0 +1,30 @@
+var a = +prompt("enter num");
+var operater = prompt("enter operater");
+var b = +prompt("enter num");
+
+function s(){
+    if(operater =="+"){
+        var cal = a + b;
+        return cal;
+    }else if
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
